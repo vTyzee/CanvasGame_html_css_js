712830214a5cf4204d900bb1969f3639e6fd0698
Ekaterina Lyamina <svetlanka65@inbox.ru>
2024-12-13 09:35:38 +0200

    task 6

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+const KEYS = {
+    LEFT: 37,
+    RIGHT: 39
+};
+
 let game = {
     ctx: null,
     platform: null,
@@ -17,14 +22,12 @@ let game = {
     },
     setEvents() {
         window.addEventListener("keydown", e => {
-            if (e.keyCode === 37) {
-                this.platform.dx = -this.platform.velocity;
-            } else if (e.keyCode === 39) {
-                this.platform.dx = this.platform.velocity;
+            if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+                this.platform.start(e.keyCode);
             }
         });
         window.addEventListener("keyup", e => {
-            this.platform.dx = 0;
+            this.platform.stop();
         });
     },
     preload(callback) {
@@ -66,7 +69,7 @@ let game = {
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
-                           this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+            this.ball.x, this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
         this.renderBlocks();
     },
@@ -83,3 +86,37 @@ let game = {
         });
     }
 };
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    velocity: 6,
+    dx: 0,
+    x: 280,
+    y: 300,
+    start(direction) {
+        if (direction === KEYS.LEFT) {
+            this.dx = -this.velocity;
+        } else if (direction === KEYS.RIGHT) {
+            this.dx = this.velocity;
+        }
+    },
+    stop() {
+        this.dx = 0;
+    },
+    move() {
+        if (this.dx) {
+            this.x += this.dx;
+            game.ball.x += this.dx;
+        }
+    }
+};
+
+window.addEventListener("load", () => {
+    game.start();
+});
